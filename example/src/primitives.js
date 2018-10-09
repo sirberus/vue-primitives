df6212ae8aa6433a9fcfdc1559d2c532eb1b80b9
PK <sirberus.io@gmail.com>
2018-10-08 21:01:08 -0700

    globally register

---
+++ example/src/primitives.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { registerPrimitive } from '../../src/index.js'
+
+registerPrimitive(Vue, 'div', 'box')
+registerPrimitive(Vue, 'span', 'txt')
+registerPrimitive(Vue, 'hr', 'bar', ['thick'])
