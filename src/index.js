bf7c26f4b07923528c79ca78890551cea640c9ef
PK <sirberus.io@gmail.com>
2018-10-08 21:00:59 -0700

    passthrough

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import { createPrimitive }  from './createPrimitive.js'
-export *  from './createPrimitive.js'
+import cp  from './createPrimitive.js'
+import rp from './registerPrimitive.js'
+
+export const createPrimitive = cp
+export const registerPrimitive = rp
