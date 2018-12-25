e99589bf43d1a5cf261a257dc1d4e852cf2dc42c
Patrick Keenan <sirberus.io@gmail.com>
2018-12-24 19:39:26 -0800

    version without primitives for example

---
+++ example/src/plain/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-wrapper min-h-screen" 
+    :class="{ 'bg-black': $store.getters.dark, 'bg-white': !$store.getters.dark }"
+  >
+    <div class="app-header bg-green h-16 flex items-center md:pl-10">
+      <span class="font-bold text-3xl"
+        :class="{ 'text-black': $store.getters.dark, 'text-white': !$store.getters.dark }"
+      >Vue Primitives</span>
+    </div>
+    <app-content/>
+  </div>
+</template>
+
+<script>
+import AppContent from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    AppContent
+  }
+}
+</script>
