6822f9b3065c17706ce3619851a3da3c1ad39e7d
Patrick Keenan <sirberus.io@gmail.com>
2019-01-05 22:38:26 -0800

    update example

--- example/src/components/AppContent.vue
+++ example/src/components/AppContent.vue
@@ -1,19 +1,20 @@
 <template>
   <page p-2 md:p-10>
     <container>
+      
       <headline>Hello World!</headline>
       <txt>This was made with vue-primitives and Tailwind CSS</txt>
       <darkmode-toggler w-64 mb-12 @click="$store.commit('toggle-dark')"/>
       
-      <headline>Check out AppContent.vue for best examples</headline>
+      <headline>Check out <code>AppContent.vue</code> and <code>primitives.js</code> for best examples</headline>
       <txt mb-12>The benefits are much more clear when the elements are used more than once.</txt>
       
-      <headline>Remember, this is just a dumb one-page app</headline>
-      <txt mb-12>For scaffoling a proper application, you could separate conditionals into utility files and use global registration to keep the &lt;script&gt; block clean.</txt>
-    
       <headline>Why does the example not use global registration?</headline>
-      <txt>You'll want to develop with hot reloading, but changes to global registrations always trigger a full reload. It's better practice to keep registration local until the API is cemented and if you use the element in other files.</txt>
-      <txt>Take care to not globally register everything. If it's only used in one file, keep it in that file.</txt>
+      <txt mb-12>You'll want to develop with hot reloading, but changes to global registrations always trigger a full reload. It's better practice to keep registration local until the API is cemented and if you use the element in other files.</txt>
+    
+      <headline>Lorem ipsum dolor sit amet</headline>
+      <txt mb-12>Nunc orci est, malesuada non ligula quis, facilisis malesuada lectus. Curabitur sed odio non sapien tincidunt rutrum. Ut cursus ut magna vitae feugiat. Praesent et lectus tortor. Aenean pharetra magna id nisi tempus consectetur. Cras eget felis nisi. In ut turpis a ex aliquam finibus. Vestibulum vitae tellus hendrerit, rutrum nunc sed, imperdiet ipsum. Donec mauris sem, gravida id vestibulum ac, vehicula non ipsum. Maecenas porta sapien auctor volutpat sollicitudin. Vivamus ut velit non nisl ultrices hendrerit sit amet eu nunc. Nunc tincidunt dignissim consequat. Integer non ornare orci. Duis vel congue elit, ut suscipit ligula.</txt>
+    
     </container>
   </page>
 </template>
--- example/src/primitives.js
+++ example/src/primitives.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import { createPrimitive } from '../../src/index.js'
-// import { createPrimitive } from 'vue-primitives'
+import { registerPrimitives } from '../../src/index.js'
 
 // Just an example of global registration. This isn't used in the application.
-Vue.component('column', createPrimitive({ name: 'Column', tag: 'div', classes: 'flex flex-col' }))
+registerPrimitives(Vue, {
+  Column: { tag: 'div', classes: 'flex flex-col' }
+})
